5c0005f6b8bcb0ebccc2bc2783cbf5910805b99c
Sergey Danilchuk <sergeydanilchuk77@gmail.com>
2025-11-10 15:16:12 +1000

    add export for /admin & /settings

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,3 +1,8 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
+
+
 import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/lib/getCurrentUser";
 import { Card } from "@/components/ui/card";
